26a08ba0094005f381dac7c5d905c48fc92f34b9
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-03-24 08:31:41 -0600

    units of 25

--- src/main/java/world/ntdi/projectapi/life/lives/JavaSHIT.java
+++ src/main/java/world/ntdi/projectapi/life/lives/JavaSHIT.java
@@ -4,6 +4,6 @@ import world.ntdi.projectapi.life.Life;
 
 public class JavaSHIT extends Life {
     public JavaSHIT() {
-        super("CANVAS.js", "I made.. ..100ish games with JS Canvas, most being bullet hell games. Anyways I learned a lot of Javascript fundamentals which as a new programmer, was really good to learn. I really enjoyed game development and discovered the game engine called Unity.", 45);
+        super("CANVAS.js", "I made.. ..100ish games with JS Canvas, most being bullet hell games. Anyways I learned a lot of Javascript fundamentals which as a new programmer, was really good to learn. I really enjoyed game development and discovered the game engine called Unity.", 25);
     }
 }
--- src/main/java/world/ntdi/projectapi/life/lives/MinecraftPlugins.java
+++ src/main/java/world/ntdi/projectapi/life/lives/MinecraftPlugins.java
@@ -4,6 +4,6 @@ import world.ntdi.projectapi.life.Life;
 
 public class MinecraftPlugins extends Life {
     public MinecraftPlugins() {
-        super("SPIGOT.java", "Minecraft is a game played by many, and I loved it. I loved making servers with redstone and command blocks, but soon realized that if I wanted to give my players a professional grade experience, I was going to have to step it up a notch. I went back into the Java field and learned about the Spigot API, which is an API for interacting with a Minecraft Server, allowing you to make mostly whatever you want. The Spigot API is how I learned some more advanced concepts in Java and even go into hosting my own JVM libraries! Yet, how am I to display off all these projects without a little frontend help?", 66);
+        super("SPIGOT.java", "Minecraft is a game played by many, and I loved it. I loved making servers with redstone and command blocks, but soon realized that if I wanted to give my players a professional grade experience, I was going to have to step it up a notch. I went back into the Java field and learned about the Spigot API, which is an API for interacting with a Minecraft Server, allowing you to make mostly whatever you want. The Spigot API is how I learned some more advanced concepts in Java and even go into hosting my own JVM libraries! Yet, how am I to display off all these projects without a little frontend help?", 50);
     }
 }
